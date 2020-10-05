88f85e2d97768b3fdd943b10a3209c9141d2330a
ruan.eloy <ruan.eloy@thoughtworks.com>
2020-10-05 09:16:57 -0300

    create signin page form

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import {
-  View, StyleSheet, Text, Button,
+  View, StyleSheet,
 } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
 
 const SigninScreen = ({ navigation }) => (
-  <View>
-    <Text style={styles.text}>Signin</Text>
-    <Button onPress={() => navigation.navigate('Signup')} title="Go to Signup" />
+  <View style={styles.container}>
+    <Text h3>Signin</Text>
+    <Input label="E-mail" />
+    <Input label="Password" />
+    <Button onPress={() => navigation.navigate('Signup')} title="Signin" />
   </View>
 );
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 40,
+  container: {
+    flex: 1,
+    padding: 10,
+    alignItems: 'center',
+    alignContent: 'center',
+    justifyContent: 'center',
+    marginBottom: 200,
   },
+});
+
+SigninScreen.navigationOptions = () => ({
+  headerShown: false,
 });
 
 export default SigninScreen;
