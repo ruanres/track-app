0528933708b84485676ed78efc90ea14bed5314a
ruanres <ruan.silveira@ccc.ufcg.edu.br>
2020-10-06 22:34:03 -0300

    set inputs props

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, Input, Button } from 'react-native-elements';
 import Container from '../components/Container';
 
-const SigninScreen = ({ navigation }) => (
-  <Container>
-    <Text h3>Signin</Text>
-    <Input label="E-mail" />
-    <Input label="Password" />
-    <Button onPress={() => navigation.navigate('Signup')} title="Signin" />
-  </Container>
-);
+const SigninScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <Container>
+      <Text h3>Signin</Text>
+      <Input
+        label="E-mail"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <Input
+        label="Password"
+        value={password}
+        onChangeText={setPassword}
+        autoCapitalize="none"
+        autoCorrect={false}
+        secureTextEntry
+      />
+      <Button onPress={() => navigation.navigate('Signup')} title="Signin" />
+    </Container>
+  );
+};
 
 SigninScreen.navigationOptions = () => ({
   headerShown: false,
